c6f4ff210669adec44400d598a9913f1e3942038
Lexer747 <Lexer747@gmail.com>
2017-01-17 21:03:11 +0000

    cleaned code, added more colour options

--- Base.hs
+++ Base.hs
@@ -1,13 +1,16 @@
 import Numeric (showHex)
 import Data.Char (toUpper)
 
-type Point          = (Float, Float)
-type Colour         = (Int,Int,Int)
+
+type Colour         = (Int,Int,Int) --line colour of a shape
 type Fill           = (Int,Int,Int,Int,Int,Int)
-type Polygon        = [Point] --ORDER MATTERS!!
-type Figure         = [Polygon]
-type FigureC        = [(Colour, Polygon)]
-type Transformation = (Float, Float, Float, Float, Float, Float)
+type Point          = (Float, Float) --generic point of polygon
+type Polygon        = [Point] --generic shape
+-- ^^^ORDER MATTERS!!^^^
+type Figure         = [Polygon] --a figure contains a list of shapes
+type FullPolygon    = (Fill, Colour, Polygon) --a fully coloured shape
+type FullFigure     = [FullPolygon] --a FullFigure is a list of fully coloured shapes
+type Transformation = (Float, Float, Float, Float, Float, Float) --an afine transformation matrix
 
 --useage: writePoint Point => svg String
 writePoint :: Point -> String 
@@ -17,31 +20,23 @@ writePoint (x,y) = (show x)++","++(show y)++" "
 writePolygon :: (Colour,Polygon) -> String 
 writePolygon ((r,g,b),p) = "<polygon points=\""++(concatMap writePoint p)++"\" style=\"fill:#cccccc;stroke:rgb("++(show r)++","++(show g)++","++(show b)++");stroke-width:2\"/>"
 
-writeFillPolygon :: (Fill, Colour, Polygon) -> String
-writeFillPolygon ((x1,x2,x3,x4,x5,x6),(r,g,b),p) = "<polygon points=\""++(concatMap writePoint p)++"\" style=\"fill:#"++(show x1)++(show x2)++(show x3)++(show x4)++(show x5)++(show x6)++";stroke:rgb("++(show r)++","++(show g)++","++(show b)++");stroke-width:2\"/>"
+writeFullPolygon :: FullPolygon -> String
+writeFullPolygon ((x1,x2,x3,x4,x5,x6),(r,g,b),p) = 
+    "<polygon points=\""++(concatMap writePoint p)++"\" style=\"fill:#"++(f)++";stroke:rgb("++(show r)++","++(show g)++","++(show b)++");stroke-width:2\"/>" where
+    f = (writeHex x1)++(writeHex x2)++(writeHex x3)++(writeHex x4)++(writeHex x5)++(writeHex x6)
 
---useage: writeFigure [(Colour,Polygon)] => svg String
-writeFigure :: FigureC -> String 
-writeFigure p = "<svg xmlns=\"http://www.w3.org/2000/svg\">"++(concatMap writePolygon p)++"</svg>"
-
-blank :: Colour
-blank = (200,200,200)
-
+--useage: writeFullFigure FullFigure => svg String
+writeFullFigure :: FullFigure -> String 
+writeFullFigure p = "<svg xmlns=\"http://www.w3.org/2000/svg\">"++(concatMap writeFullPolygon p)++"</svg>"
 
 writeHex :: Int -> String
 writeHex x = map toUpper (showHex x "")
 
---useage: writePolygonColourless Polygon => svg String
-writePolygonColourless :: Polygon -> String
-writePolygonColourless p = writePolygon (blank, p)
+colorizeFig :: Fill -> Colour -> Figure -> FullFigure
+colorizeFig fill line fig = zip3 (repeat fill) (repeat line) fig
 
---useage: writeFigureColourless Figure => svg String
-writeFigureColourless :: Figure -> String
-writeFigureColourless p = writeFigure $ colorize blank p
 
-colorize :: Colour -> Figure -> FigureC
-colorize = zip.repeat
-
+--infix transformation for transfroming points
 (%%) :: Point -> Transformation -> Point
 (%%) (x, y) (a, b, c, p, q, r) = 
     ((x * a + y * b + c),(x * p + y * q + r))
@@ -54,7 +49,7 @@ scale w h = (w,0,0,0,h,0)
 
 rot :: Float -> Transformation
 rot theata = (cos x, sin x, 0, -sin x, cos x, 0) where
-    x = theata * (180 / pi)
+    x = theata -- * (180 / pi)
 
 shear :: Float -> Float -> Transformation
 shear phi psi = (1,tan x,0,tan y,1,0) where
@@ -64,50 +59,44 @@ shear phi psi = (1,tan x,0,tan y,1,0) where
 folding acc f [] = acc
 folding acc f (x:xs) = folding (f acc x) f xs
     
-    
+--useage: transformPolygon [list of transformations to do] Polygon => Polygon
 transformPolygon :: [Transformation] -> Polygon -> Polygon
 transformPolygon trList poly = map f poly where
     f = (\x -> folding (x) (%%) trList)
     
+--useage: same as above except it uses a fully coloured shape
+transformFullPoly :: [Transformation] -> FullPolygon -> FullPolygon
+transformFullPoly trList (f,c,poly) = (f,c,(transformPolygon trList poly))
+
+changeColour :: (Fill, Colour) -> FullPolygon -> FullPolygon
+changeColour (newF,newC) (oldF,oldC,poly) = (newF, newC, poly)
+
+
+
+findMaxFull :: FullPolygon -> FullPolygon
+findMaxFull (f,c,poly) = (f,c,(findMax poly))
+
 findMax :: Polygon -> Polygon
 findMax p = findMax_help $ unzip p
 
 findMax_help :: ([Float],[Float]) -> Polygon
 findMax_help (x,y) = [(minimum x, minimum y),(minimum x, maximum y), (maximum x, maximum y),(maximum x, minimum y)]
     
+--generic line colour
+blueL :: Colour
+blueL = (0,0,255)
 
+--generic fill colour
+blueF :: Fill
+blueF = (0,0,0,0,15,15)
+
+--generic square
 square :: Polygon
 square = [(0,0),(0,100),(100,100),(100,0)]
 
-outputColourlessShape :: Polygon -> IO ()
-outputColourlessShape shape = writeFile "svg/Output.svg" $ writeFigureColourless [shape]
+--blue square
+blueSquare :: FullPolygon
+blueSquare = (blueF, blueL, square)
 
-outputFigure :: FigureC -> IO ()
-outputFigure x = writeFile "svg/Output.svg" $ writeFigure x
-
-{-
-rainbow@[red,green,blue,yellow,purple,teal] = map colorize [(255,0,0),(0,255,0),(0,0,255),(255,255,0),(255,0,255),(0,255,255)]
-
-t0 = writeFile "tut0.svg" $ writePolygons (blue [[(100,100),(200,100),(200,200),(100,200)],[(200,200),(300,200),(300,300),(200,300)]])
-
-hexagon c r = translateTo c basicHexagon where
-  basicHexagon =  top ++ (negate r, 0):bottom 
-  top          =  [(r,0),(r * cos 1,(r * sin 1)),(negate (r * cos 1), r * (sin 1))]
-  bottom       =  map (\(x,y)->(x,negate y)) (reverse top)
-
-translateTo (x,y) poly = map f poly where f (a,b) = ((a+x),(b+y))
-
-
-t1 = writeFile "t1.svg" $ writePolygons (blue [hexagon (100,100) 50] )
-
-hexField r n m = let 
-     mkHex n = hexagon (1.5*n*(r*2),(r*2)) r
-     row n = map mkHex [1..n]
-     aRow = row n
-  in concat [map (offset (r*x)) aRow |x<-[1..m]]
-
-offset r polys = map (oh r) polys where
-  oh r pt@(x,y) = (x+(1.5*r),y+(r*sin 1))
-
-t2 = writeFile "t2.svg" $ writePolygons (blue $ hexField 50 4 5 )
--}
+outputFullFigure :: FullFigure -> IO ()
+outputFullFigure fig = writeFile "svg/Output.svg" $ writeFullFigure fig
