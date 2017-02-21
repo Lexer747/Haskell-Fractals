1c85d07ef76ea4319fadb2eb945d6b37dc42fc0a
Lexer747 <Lexer747@gmail.com>
2017-02-21 01:25:54 +0000

    undo reset

--- Core/Mandelbrot.hs
+++ Core/Mandelbrot.hs
@@ -27,7 +27,7 @@ mandelXmax (x,_) = (x + gap)
 mandelYmin (_,y) = (y - gap)
 mandelYmax (_,y) = (y + gap)
 
-customMandelFunc :: Point -> Float -> (Pixel -> Pixel)
+--customMandelFunc :: Point -> Float -> (Pixel -> Pixel)
 customMandelFunc base zoom pixel = Pixel (location pixel) representative where
     representative = mandelColour iter
     iter = applyMandel iterations curPoint 
@@ -40,7 +40,7 @@ mandelbrotFunc pixel = Pixel (location pixel) representative where
     iter = applyMandel iterations curPoint
     curPoint = normalizePixel pixel
 
-mandelColour :: (Integral a) => a -> Outline
+--mandelColour :: (Integral a) => a -> Outline
 mandelColour iter = if (fromIntegral iter) == iterations then (0,0,0) else (r,g,b) where
     r = round $ t * ( 1 + sin t) / 1
     g = round $ t * ( 1 + cos t) / 1
@@ -48,29 +48,30 @@ mandelColour iter = if (fromIntegral iter) == iterations then (0,0,0) else (r,g,
     t = normalize (sqrt brightness) 0 1 15 255 --240
     brightness = normalize (fromIntegral iter) 0 (fromIntegral iterations) 0 1
     
-applyMandel :: Int -> Point -> Int
-applyMandel n (x,y) = n - applyMandel_help n (x,y) (x,y)
-    
+
 --applyMandel will take a number of function iterations to do and return how many it managed to perform on a point
-applyMandel_help :: (Floating t, Ord t) => Int -> (t,t) -> (t,t) -> Int
+applyMandel :: Int -> Point -> Int
+applyMandel n (x,y) = n - applyMandel_help n (x,y) (0,0)
+    
+--applyMandel_help :: (Floating t, Ord t) => Int -> (t,t) -> (t,t) -> Int
 applyMandel_help 0 _ _ = 0
 applyMandel_help n (baseX,baseY) (x,y) = 
-    if  (sqrt $ (newX * newX) + (newY * newY)) > 16
+    if  (sqrt $ (newX * newX) + (newY * newY)) > 2
         then n
         else (applyMandel_help (n-1) (baseX,baseY) (newX,newY)) where
             newX = baseX +((x * x) - (y * y))
             newY = baseY + (2 * x * y)
 
-normalize :: Fractional a => a -> a -> a -> a -> a -> a
+--normalize :: Fractional a => a -> a -> a -> a -> a -> a
 normalize cur min max newMin newMax = (((newMax - newMin) * (cur - min)) / (max - min)) + newMin
 
-normalizePixel :: Fractional t => Pixel -> (t,t)
+--normalizePixel :: Fractional t => Pixel -> (t,t)
 normalizePixel pixel = (newX, newY) where
     (x,y) = location pixel
     newX = normalize (fromIntegral x) 0 (fromIntegral width) (mandelXmin basePoint) (mandelXmax basePoint)
     newY = normalize (fromIntegral y) 0 (fromIntegral height) (mandelYmin basePoint) (mandelYmax basePoint)
 
-customNormalizePixel :: Fractional t => (t,t) -> t -> Pixel -> (t,t)
+--customNormalizePixel :: Fractional t => (t,t) -> t -> Pixel -> (t,t)
 customNormalizePixel (a,b) zoom pixel = (newX,newY) where
     (x,y) = location pixel
     newX = normalize (fromIntegral x) 0 (fromIntegral width) (a - gap) (a + gap)
@@ -81,10 +82,10 @@ customNormalizePixel (a,b) zoom pixel = (newX,newY) where
 mandelbrotSet :: FullFigure
 mandelbrotSet = convertGrid $ mapGrid mandelbrotFunc $ buildGrid height width whitePixel
 
-specificMandelSet :: Point -> Float -> FullFigure
+--specificMandelSet :: Point -> Float -> FullFigure
 specificMandelSet base zoom = convertGrid $ mapGrid (customMandelFunc base zoom) (buildGrid height width whitePixel)
 
-mandelZoom :: Point -> Float -> (Float -> Float) -> Int -> [FullFigure]
+--mandelZoom :: Point -> Float -> (Float -> Float) -> Int -> [FullFigure]
 mandelZoom _ _ _ 0                      = []
 mandelZoom base startzoom zoomFunc iter = (image):(mandelZoom base (zoomFunc startzoom) zoomFunc (iter - 1)) where
     image = specificMandelSet base startzoom
